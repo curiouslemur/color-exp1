d485ff091b3843e7e18ed4707090a9dc8a755d03
Noëlle R <noelle.tiana@gmail.com>
2024-01-08 14:18:15 -0800

    test secrets

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import './App.css';
+import Button from '@mui/material/Button';
+
+
+const onClick = () => {
+  alert("hello " + process.env.REACT_APP_APIKEY)
+}
 
 function App() {
   return (
@@ -6,6 +12,7 @@ function App() {
       <header className="App-header">
         Here starts the app. The secret is: <br />
         <b>{process.env.REACT_APP_TEST_SECRET}</b>
+        <Button onClick={onClick}>Click me</Button>
       </header>
     </div>
   );
