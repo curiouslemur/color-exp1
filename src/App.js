59d0c8d69692cac7f69e2ec6b510bc45ef463d2f
Noëlle R <noelle.tiana@gmail.com>
2024-01-08 13:05:43 -0800

    adding homepage

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Here starts the app
+        Here starts the app. The secret is: <br />
+        <b>{process.env.REACT_APP_TEST_SECRET}</b>
       </header>
     </div>
   );
