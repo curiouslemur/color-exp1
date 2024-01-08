b71b23dc3a8763b5269406391ab485dad4710025
Noëlle R <noelle.tiana@gmail.com>
2024-01-08 15:05:34 -0800

    test fs

--- src/_utils/firebase-config.js
+++ src/_utils/firebase-config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { addDoc, collection, doc, getFirestore, setDoc } from 'firebase/firestore'
+import { doc, getFirestore, setDoc } from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_APIKEY,
