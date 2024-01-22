ef51ab55ebc942ce5066f651f2f736617bb91fac
Noëlle R <noelle.tiana@gmail.com>
2024-01-22 10:58:13 -0500

    .

--- src/_components/outro.jsx
+++ src/_components/outro.jsx
@@ -6,19 +6,27 @@ import '../App.css'
 const styles = {
     button: { marginTop: 10, marginBottom: 10 },
     container: { display: 'flex', flexWrap: 'wrap', padding: '1%' },
+    gridItem: { display: 'flex', justifyContent: 'center', alignItems: 'center' },
     root: { flexGrow: 1, margin: '2%' },
     textField: { marginLeft: 10, marginRight: 10, width: 200, }, label: { margin: 0 }
 }
 
 export const Outro = (props) => {
     const labels = props.expPages.OutroLabels
-    oc.isProlificUser()
+
+    console.log(oc.isProlificUser())
     return (
         <Grid container style={styles.container} justifyContent='center'>
-            <Typography variant="h4">{labels.outroTitle}</Typography>
-            {oc.isProlificUser ? labels.prolificUser1 : labels.closeBrowser}
-            {oc.isProlificUser ? <Button href="www.youtube.com">{labels.prolificUserButton}</Button> : <></>}
-        </Grid>
+            <Grid item xs={10} sm={8} xl={8} style={styles.gridItem} marginTop={2}>
+                <Typography variant="h5">{labels.outroTitle}</Typography>
+            </Grid>
+            <Grid item xs={10} sm={8} xl={8} style={styles.gridItem} marginTop={2}>
+                <Typography> {oc.isProlificUser === true ? labels.prolificUser1 : labels.closeBrowser} </Typography>
+            </Grid>
+            <Grid item xs={10} sm={8} xl={8} style={styles.gridItem} marginTop={2}>
+                {oc.isProlificUser ? <Button target="_blank" href="https://www.youtube.com">{labels.prolificUserButton}</Button> : <></>}
+            </Grid>
+        </Grid >
     )
 }
 
--- src/_controllers/outroController.js
+++ src/_controllers/outroController.js
@@ -1,4 +1,5 @@
 export const isProlificUser = () => {
     let dem = JSON.parse(sessionStorage.getItem('demography'))
+    console.log(dem, dem.prolificID.length)
     if (dem.prolificID.length > 0) { return true } else { return false }
 }
