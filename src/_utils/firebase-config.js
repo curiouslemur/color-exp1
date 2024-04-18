63e38f0095fe830b2c6f69ff8e7b1fe177030b91
Noëlle R <noelle.tiana@gmail.com>
2024-04-18 13:49:05 -0700

    added second alt firestore

--- src/_utils/firebase-config.js
+++ src/_utils/firebase-config.js
@@ -10,6 +10,15 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_APPID
 }
 
+const firebaseConfigAlt = {
+    apiKey: process.env.REACT_APP_APIKEY_ALT,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN_ALT,
+    projectId: process.env.REACT_APP_PROJECTID_ALT,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET_ALT,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDERID_ALT,
+    appId: process.env.REACT_APP_APPID_ALT
+}
+
 // initialize firebase
 const app = initializeApp(firebaseConfig)
 
