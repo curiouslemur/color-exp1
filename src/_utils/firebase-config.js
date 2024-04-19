2dbfa22504345a47f43b1a9cadef7d7be12eeb99
Noëlle R <noelle.tiana@gmail.com>
2024-04-19 16:01:02 -0700

    .

--- src/_utils/firebase-config.js
+++ src/_utils/firebase-config.js
@@ -10,15 +10,6 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_APPID
 }
 
-// const firebaseConfig_ALT = {
-//     apiKey: process.env.REACT_APP_APIKEY_ALT,
-//     authDomain: process.env.REACT_APP_AUTHDOMAIN_ALT,
-//     projectId: process.env.REACT_APP_PROJECTID_ALT,
-//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET_ALT,
-//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDERID_ALT,
-//     appId: process.env.REACT_APP_APPID_ALT
-// }
-
 // initialize firebase
 const app = initializeApp(firebaseConfig)
 
